1e2472d5e9c1817bc6f1820269cb51158c799f66
zhangguobin <1774427221@qq.com>
2020-01-14 13:36:58 +0800

    正序、倒序打印小星星

--- src/main/java/algorithm/algs.java
+++ src/main/java/algorithm/algs.java
@@ -3,13 +3,50 @@ package algorithm;
 public class algs {
 
 	public static void main(String[] args) {
-		String a ="*";
-		for(int i=1;i<9;i++){
+	printing1("*",5);
+
+	}
+	/**
+	 * @param 需要打印的符号
+	 * @param 打印几行
+	 * 正序
+	 * *
+	 * **
+	 * ***
+	 */
+	public static void printing(String str,int str1){
+		for(int i=1;i<=str1;i++){
 			for(int j=1;j<=i;j++){
-		System.out.print(a);
+		System.out.print(str);
 		}
 			System.out.println(" ");
 		}
 	}
-
+	
+	/**
+	 * @param 需要打印的符号
+	 * @param 打印几行
+	 * 倒序
+	 * ***
+	 * **
+	 * *
+	 */
+	public static void printing1(String str,int str1){
+		for(int i=1;i<=str1;i++){
+			for(int j=i;j<=str1;j++){
+		System.out.print(str);
+		}
+			System.out.println(" ");
+		}
+	}
+	
+	public static void printing2(String str,int str1){
+		for(int i=1;i<=str1;i++){
+			for(int j=i;j<=str1;j++){
+				System.out.print(" ");		
+		System.out.print(str);
+		}
+			System.out.println(" ");
+		}
+	}
 }
